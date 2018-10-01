ef19a2dbc4cec32d75a761bd641446413ff383ec
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2018-10-01 11:27:21 +0200

    added flask server

--- cartpole.py
+++ cartpole.py
@@ -1,63 +1,44 @@
 import gym
 import numpy as np
-from flask import Flask
-import json
 
-env = gym.make("cartpole-v1")
+env = gym.make('CartPole-v1')
 
 
 def play(env, policy):
     observation = env.reset()
+
     done = False
-    observations = []
     score = 0
+    observations = []
 
     for _ in range(5000):
-        observations += [observation.tolist()]
+        observations += [observation.tolist()]  # Record the observations for normalization and replay
 
-        if done:
+        if done:  # If the simulation was over last iteration, exit loop
             break
 
-    #     Pick an action according to the policy matrix
+        # Pick an action according to the policy matrix
+        outcome = np.dot(policy, observation)
+        action = 1 if outcome > 0 else 0
 
-    outcome = np.dot(policy, observation)
-    action = 1 if outcome > 0 else 0
-    #      Make action, record reward
-    observation, reward, done, info = env.step(action)
-    score += reward
-    return score, observation
+        # Make the action, record reward
+        observation, reward, done, info = env.step(action)
+        score += reward
 
+    return score, observations
 
-policy = np.random.rand(1, 4) - 0.5
-
-score, observations = play(env, policy)
-print('Policy Score', score)
-
-app = Flask(__name__, static_folder='.')
-
-
-@app.route("/data")
-def data():
-    return json.dump(max[1])
-
-
-@app.route("/")
-def root():
-    return app.send_static_file('./index.html')
-
-
-app.run(host='0.0.0.0', port='3000')
 
 max = (0, [], [])
 
+# We changed the next two lines!
 for _ in range(100):
-    policy = np.random.rand(1, 4)
+    policy = np.random.rand(1, 4) - 0.5
     score, observations = play(env, policy)
 
     if score > max[0]:
         max = (score, observations, policy)
 
-    print('Max score:', max[0])
+print('Max Score', max[0])
 
 scores = []
 for _ in range(100):
@@ -65,3 +46,23 @@ for _ in range(100):
     scores += [score]
 
 print('Average Score (100 trials)', np.mean(scores))
+
+
+
+from flask import Flask
+import json
+
+app = Flask(__name__, static_folder='.')
+
+
+@app.route("/data")
+def data():
+    return json.dumps(max[1])
+
+
+@app.route('/')
+def root():
+    return app.send_static_file('./index.html')
+
+
+app.run(host='localhost', port='3000')
